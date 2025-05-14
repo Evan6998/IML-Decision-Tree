b62b38d7353a0c683108be1c1d16bba44625302c
Evan6998 <hyf6998@icloud.com>
2025-05-14 18:51:43 -0400

    pretty print

--- decision_tree.py
+++ decision_tree.py
@@ -22,6 +22,7 @@ class Node:
         # self.left: Node | None = None
         # self.right: Node | None = None
         self.children: dict[FeatureType, Node] = {}
+        self.distribution: dict[FeatureType, int] = {}
 
         self.attr: typing.SupportsIndex | None = None
         self.vote: LabelType | None = None
@@ -123,6 +124,19 @@ def best_attribute(dataset: np.ndarray[typing.Any, typing.Any]) -> typing.Suppor
     return best_idx
 
 
+def calculate_distribution(dataset: np.ndarray[typing.Any, typing.Any]):
+    set_of_label: set[int] = set([0,1])
+    label = dataset[:, -1]
+    unique, counts = np.unique(label, return_counts=True)
+    from collections import OrderedDict
+    result = dict(zip(unique, counts))
+    # print(f"{set_of_label=}, {set(result.keys())=}")
+    for y in set_of_label - set(result.keys()):
+        result[y] = 0
+    # print(result)
+    return OrderedDict(sorted(result.items()))
+
+
 def tree_recurse(
     dataset: np.ndarray[typing.Any, typing.Any], cur_depth: int, max_depth: int
 ) -> Node:
@@ -140,6 +154,7 @@ def tree_recurse(
             # q.children[edge] = tree_recurse(D_v, cur_depth + 1, max_depth)
             q.children[v] = tree_recurse(D_v, cur_depth + 1, max_depth)
     q.vote = mode_of_label(dataset)
+    q.distribution = calculate_distribution(dataset)
     # q.vote = mapping[4][mode_of_label(dataset)]
     return q
 
@@ -157,10 +172,16 @@ def predict(root: Node, x: np.ndarray[typing.Any, typing.Any]):
         else:
             # leaf node
             return current_node.vote
-        
-def error_rate(predict: np.ndarray[typing.Any, typing.Any], label: np.ndarray[typing.Any, typing.Any]) -> float:
-    assert predict.shape == label.shape, "Predictions and labels must have the same shape"
-    return float(1.0 - np.mean(predict == label)) # type: ignore
+
+
+def error_rate(
+    predict: np.ndarray[typing.Any, typing.Any],
+    label: np.ndarray[typing.Any, typing.Any],
+) -> float:
+    assert predict.shape == label.shape, (
+        "Predictions and labels must have the same shape"
+    )
+    return float(1.0 - np.mean(predict == label))  # type: ignore
 
 
 def predict_all(root: Node, filename: str):
@@ -172,8 +193,29 @@ def predict_all(root: Node, filename: str):
     return result, error_rate(np.array(result), dataset[:, -1])
 
 
-def print_tree(node: Node):
-    pass
+def generate_tree(
+    node: Node,
+    header: list[str],
+    last_feature: typing.SupportsIndex | None,
+    last_val: FeatureType | None,
+    depth: int,
+) -> typing.Generator[str, None, None]:
+    def stringify_distribution(distribution: dict[FeatureType, int]):
+        temp_list: list[str] = []
+        for y, count in distribution.items():
+            temp_list.append(f"{count} {int(y)}")
+        output_string = f"[{'/'.join(temp_list)}]"
+        return output_string
+
+    if last_feature is None or last_val is None:
+        # root
+        output_string = stringify_distribution(node.distribution)
+        yield output_string
+    else:
+        output_string = f"{'| ' * depth}{header[last_feature]} = {int(last_val)}: {stringify_distribution(node.distribution)}"
+        yield output_string
+    for feature_val, child in node.children.items():
+        yield from generate_tree(child, header, node.attr, feature_val, depth + 1)
 
 
 if __name__ == "__main__":
@@ -221,21 +263,23 @@ if __name__ == "__main__":
 
     result, train_err = predict_all(root, args.train_input)
     print(f"error(train): {train_err}")
-    with open(args.train_out, 'w') as fout:
+    with open(args.train_out, "w") as fout:
         for label in result:
             fout.write(str(label))
-            fout.write('\n')
+            fout.write("\n")
 
     result, test_err = predict_all(root, args.test_input)
     print(f"error(test): {test_err}")
-    with open(args.test_out, 'w') as fout:
+    with open(args.test_out, "w") as fout:
         for label in result:
             fout.write(str(label))
-            fout.write('\n')
+            fout.write("\n")
 
-    with open(args.metrics_out, 'w') as fout:
+    with open(args.metrics_out, "w") as fout:
         fout.write(f"error(train): {train_err}\n")
         fout.write(f"error(test): {test_err}\n")
 
-    with open(args.print_out, 'w') as fout:
-        ...
+    with open(args.print_out, "w") as fout:
+        for s in generate_tree(root, list(np.genfromtxt(args.train_input, dtype=str, max_rows=1)), None, None, 0):
+            fout.write(s)
+            fout.write("\n")
